15e4513204633f5a1e09d6056f2cfa14792909a7
Francesco Baldassarri <fbaldassarri@expert.ai>
2022-06-19 15:20:00 +0200

    Loading Files
    Initial commit

---
+++ frame_extractor002.py
@@ -0,0 +1,28 @@
+# https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html
+
+import sys
+import argparse
+
+import cv2
+print(cv2.__version__)
+
+def extractImages(pathIn, pathOut):
+    count = 0
+    vidcap = cv2.VideoCapture(pathIn)
+    success,image = vidcap.read()
+    success = True
+    while success:
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*10))    # in this line, count means frame per millisecond [count*1 = 1 frame per millisecond]
+        success,image = vidcap.read()
+        print ('Read a new frame: ', success)
+        print ('Frame number: ', count)
+        cv2.imwrite( pathOut + "\frame%d.jpg" % count, image)     # save frame as JPEG file
+        count = count + 1
+
+if __name__=="__main__":
+    a = argparse.ArgumentParser()
+    a.add_argument("--pathIn", help="path to video")
+    a.add_argument("--pathOut", help="path to images")
+    args = a.parse_args()
+    print(args)
+    extractImages(args.pathIn, args.pathOut)
